677c1cac84d8ecc2127f0607a0cdecb8f0fbcab9
hjunlee55 <158084093+hjunlee55@users.noreply.github.com>
2025-11-11 00:06:16 +0000

    added git submodule fixes

--- create_environment.sh
+++ create_environment.sh
@@ -3,6 +3,8 @@
 python3 -m venv docking_quality_scorer
 source docking_quality_scorer/bin/activate
 pip install -r requirements.txt
+git submodule init
+git submodule update
 cd ANARCI
 python setup.py install
 cd ..
